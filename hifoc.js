0938ccbe07c5cb3def9b80038fec52012b181d5e
Ben Weedon <ben@weedon.email>
2019-12-03 23:11:39 -0800

    Add focus styling and load bookmarklet from github

--- hifoc.js
+++ hifoc.js
@@ -1,3 +1,15 @@
 javascript:(function() {
-    alert('test');
+    let style = `
+        :focus {
+            border: 2px solid cyan !important;
+            border-radius: 10px !important;
+            padding: 7px !important;
+        }
+    `;
+
+    let styleSheet = document.createElement('style');
+    styleSheet.type = 'text/css';
+    styleSheet.innerText = style;
+
+    document.head.appendChild(styleSheet);
 })();
--- loader.js
+++ loader.js
@@ -1,8 +1,9 @@
 javascript:(function() {
     let script = document.createElement('script');
     script.type = 'text/javascript';
-    script.src = '';
+    script.src = 'https://raw.githack.com/benweedon/hifoc/master/hifoc.js?v=';
     // add a random number to the URL to avoid caching
     script.src += parseInt(Math.random() * 99999999);
-    document.body.appendChild(script);
+
+    document.head.appendChild(script);
 })();
